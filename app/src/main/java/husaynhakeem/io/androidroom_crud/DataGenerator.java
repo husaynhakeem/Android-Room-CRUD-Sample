093e88741fc30066c2c54a8fc8d8efb9bcb4ada9
Husayn Hakeem <husaynhakeem@gmail.com>
2017-06-12 23:54:15 +0000

    Data generator class ready

--- app/src/main/java/husaynhakeem/io/androidroom_crud/DataGenerator.java
+++ app/src/main/java/husaynhakeem/io/androidroom_crud/DataGenerator.java
@@ -1,6 +1,9 @@
 package husaynhakeem.io.androidroom_crud;
 
+import husaynhakeem.io.androidroom_crud.database.AppDataBase;
+import husaynhakeem.io.androidroom_crud.entity.Address;
 import husaynhakeem.io.androidroom_crud.entity.Cat;
+import husaynhakeem.io.androidroom_crud.entity.Person;
 
 /**
  * Created by husaynhakeem on 6/12/17.
@@ -9,12 +12,74 @@ import husaynhakeem.io.androidroom_crud.entity.Cat;
 public class DataGenerator {
 
 
-    public static void insertCats() {
+    private static AppDataBase dataBase;
 
+
+    public static DataGenerator with(AppDataBase appDataBase) {
+        dataBase = appDataBase;
+        return new DataGenerator();
     }
 
 
-    public static Cat catInstance(String name, int age, int owner) {
+    public void generatePeople() {
+        if (dataBase == null)
+            return;
+
+        Person[] persons = new Person[4];
+        persons[0] = personInstance(1, "Husayn", "Hakeem");
+        persons[0] = personInstance(2, "Afafe", "Mokhtari");
+        persons[0] = personInstance(3, "Abderrahmane", "Bachiri");
+        persons[0] = personInstance(4, "Jalal", "Khaldouni");
+
+        dataBase.personDao().insert(persons);
+    }
+
+
+    private Address addressInstance(String street, String state, String city, int postCode) {
+        Address address = new Address();
+
+        address.street = street;
+        address.state = state;
+        address.city = city;
+        address.postCode = postCode;
+
+        return address;
+    }
+
+
+    private Person personInstance(int id, String firstName, String lastName) {
+        Person person = new Person();
+
+        person.id = id;
+        person.firstName = firstName;
+        person.lastName = lastName;
+
+        Address address = addressInstance("Some street name", "Some state", "Some city", 19421);
+        dataBase.addressDao().insert(address);
+
+        person.address = address;
+
+        return person;
+    }
+
+
+    public void generateCats() {
+
+        if (dataBase == null)
+            return;
+
+        Cat[] cats = new Cat[5];
+        cats[0] = catInstance("Tony", 3, 1);
+        cats[1] = catInstance("Tiger", 1, 1);
+        cats[2] = catInstance("Misty", 2, 2);
+        cats[3] = catInstance("Oscar", 5, 3);
+        cats[4] = catInstance("Puss", 4, 4);
+
+        dataBase.catDao().insert(cats);
+    }
+
+
+    private Cat catInstance(String name, int age, int owner) {
         Cat cat = new Cat();
 
         cat.name = name;
@@ -23,4 +88,7 @@ public class DataGenerator {
 
         return cat;
     }
+
+
+
 }
